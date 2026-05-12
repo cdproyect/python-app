a2c77e0f861599ec8a49da77b1152561060ff274
cesar.bustamante <cesar.bustamante@secloud.ca>
2026-05-12 19:14:31 -0400

    adjusting permissions

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'Hello Pika Pika! :) ',
+        'message': 'Hello Pika Pika! :) Now there is CD ',
         'deployed_on': 'kubernetes'
     })
 
